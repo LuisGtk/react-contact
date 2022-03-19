ce8f107920ba09a60fe50f40cac4cae51d6f9451
LuisGtk <luisgonzalez189@gmail.com>
2022-03-19 14:48:20 -0400

    installed react

---
+++ contact-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+{/* First comp will be the header */}
+    </div>
+  );
+}
+
+export default App;
